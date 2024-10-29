29c773ee561d24be9ed256f5bff91c62262680f0
James-Trema <andrdu29@live.fr>
2024-10-29 10:58:30 +0100

    up

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { entities } from "./entities";
+
+export const dataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "Lestania",
+    username: "postgres",
+    password: "test123",
+    logger: "advanced-console",
+    synchronize: true,
+    entities: entities
+})
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import { dataSource } from './database'
+import { router } from './routes'
 import express from 'express'
 import 'dotenv/config'
 
 const app = express()
 
+app.use('/api', router)
+
+dataSource.initialize()
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server ouvert : ${process.env.PORT}`)
+    console.log(`Server running on: ${process.env.PORT}`)
 })
