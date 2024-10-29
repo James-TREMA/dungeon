4e98bbc81fb014e019186cac1339b60e32637624
James-Trema <andrdu29@live.fr>
2024-10-29 15:18:04 +0100

    up

--- src/routes/api/chests.ts
+++ src/routes/api/chests.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { ChestsController } from "../../controllers/ChestsController";
 
 export const chestsRouter = Router();
 
-chestsRouter.get('/', async (req, res) => {
-    // Logique pour récupérer les coffres de la base de données
-    res.send("Liste des coffres"); // Remplace par la logique réelle
-});
+chestsRouter.get('/', ChestsController.getAll); // Route pour récupérer tous les coffres
+chestsRouter.get('/:id', ChestsController.getById); // Route pour récupérer un coffre par ID
+chestsRouter.post('/', ChestsController.create); // Route pour créer un nouveau coffre
+chestsRouter.put('/:id', ChestsController.update); // Route pour mettre à jour un coffre existant
+chestsRouter.delete('/:id', ChestsController.delete); // Route pour supprimer un coffre
