a756e2d01da55f55003771238cd567a10f99c856
James-Trema <andrdu29@live.fr>
2024-10-29 15:09:33 +0100

    up

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { UsersController } from "../../controllers/usersController";
 
-export const usersRouter = Router()
+export const usersRouter = Router();
 
-usersRouter.get('/', )
+// Route pour récupérer tous les utilisateurs
+usersRouter.get('/', UsersController.getAll);
+
+// Route pour récupérer un utilisateur par ID
+usersRouter.get('/:id', UsersController.getById);
+
+// Route pour créer un nouvel utilisateur
+usersRouter.post('/', UsersController.create);
+
+// Route pour mettre à jour un utilisateur existant
+usersRouter.put('/:id', UsersController.update);
+
+// Route pour supprimer un utilisateur
+usersRouter.delete('/:id', UsersController.delete);
