60e19cc1d9bbed15a4b07e219e9e8fe0e79cf1ac
James-Trema <andrdu29@live.fr>
2024-10-29 15:21:06 +0100

    up

--- src/routes/api/locations.ts
+++ src/routes/api/locations.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { LocationsController } from "../../controllers/locationsController";
 
 export const locationsRouter = Router();
 
-locationsRouter.get('/', async (req, res) => {
-    // Logique pour récupérer les emplacements de la base de données
-    res.send("Liste des emplacements"); // Remplace par la logique réelle
-});
+locationsRouter.get('/', LocationsController.getAll); // Route pour récupérer tous les emplacements
+locationsRouter.get('/:id', LocationsController.getById); // Route pour récupérer un emplacement par ID
+locationsRouter.post('/', LocationsController.create); // Route pour créer un nouvel emplacement
+locationsRouter.put('/:id', LocationsController.update); // Route pour mettre à jour un emplacement
+locationsRouter.delete('/:id', LocationsController.delete); // Route pour supprimer un emplacement
