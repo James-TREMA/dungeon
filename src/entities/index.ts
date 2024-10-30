849132d97408f902a58869098894fc32fd0931d2
James-Trema <andrdu29@live.fr>
2024-10-30 13:51:57 +0100

    up

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -4,5 +4,6 @@ import { Chest } from "./models/chests";
 import { Dungeon } from "./models/dungeons";
 import { Location } from "./models/location";
 import { Region } from "./models/regions";
+import { Achievement } from "./models/achievements";
 
-export const entities = [User, Item, Chest, Dungeon, Location, Region];
+export const entities = [User, Item, Chest, Dungeon, Location, Region, Achievement];
