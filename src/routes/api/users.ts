c3e3681bdfa0dbb436f663a83531c685d621a31d
James-Trema <andrdu29@live.fr>
2024-10-30 11:13:55 +0100

    up

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -12,5 +12,20 @@ usersRouter.get('/:id', UsersController.getById);
 // Route pour créer un nouvel utilisateur
 usersRouter.post('/', UsersController.create);
 
+// Route pour mettre à jour les attributs d'un utilisateur
+usersRouter.patch('/:id/attributes', UsersController.updateUserAttributes);
+
+// Route pour ajouter un item dans l'inventaire de l'utilisateur
+usersRouter.post('/:id/inventory', UsersController.addItemToInventory);
+
+// Route pour ajouter un ami
+usersRouter.post('/:id/friends', UsersController.addFriend);
+
+// Route pour ajouter un achievement
+usersRouter.post('/:id/achievements', UsersController.addAchievement);
+
+// Route pour mettre à jour l'utilisateur (tous les attributs)
+usersRouter.put('/:id', UsersController.updateUser);
+
 // Route pour supprimer un utilisateur
 usersRouter.delete('/:id', UsersController.delete);
